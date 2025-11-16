e73901bc67df1af8aa93960aff299ad2ed384b1d
Meraltain <obandojeremy52@gmail.com>
2025-11-16 08:44:49 -0600

    otras funciones

--- app.config.js
+++ app.config.js
@@ -3,6 +3,7 @@ import "dotenv/config";
 export default ({ config }) => ({
   ...config,
   extra: {
+    ...(config && config.extra ? config.extra : {}),
     FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
     FIREBASE_AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN,
     FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
